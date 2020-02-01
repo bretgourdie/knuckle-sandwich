49cc938298cce0c14109861c345da5711ff528f4
Bret Gourdie <BretGourdie@gmail.com>
2020-02-01 14:03:05 -0500

    Adding state framework and sprites

--- KnuckleSandwich/ContentPathGenerator.cs
+++ KnuckleSandwich/ContentPathGenerator.cs
@@ -9,7 +9,15 @@ namespace Nez
 		{
 			public static class Bread
 			{
+				public const string Attack = @"Sprites\Bread\Attack";
+				public const string Crouch = @"Sprites\Bread\Crouch";
+				public const string CrouchAttack = @"Sprites\Bread\CrouchAttack";
+				public const string Dead = @"Sprites\Bread\Dead";
+				public const string Hurt = @"Sprites\Bread\Hurt";
 				public const string Idle = @"Sprites\Bread\Idle";
+				public const string Jump = @"Sprites\Bread\Jump";
+				public const string JumpAttack = @"Sprites\Bread\JumpAttack";
+				public const string Walk = @"Sprites\Bread\Walk";
 			}
 
 			public static class Gameplay
---
+++ KnuckleSandwich/Gameplay/Components/FighterStates/FighterState.cs
@@ -0,0 +1,29 @@
+﻿using Nez;
+using Nez.Sprites;
+using System;
+using System.Collections.Generic;
+
+namespace KnuckleSandwich.Gameplay.Components.FighterStates
+{
+    abstract class FighterState : Component
+    {
+        public abstract Type HandleInput(VirtualAxis xAxis, VirtualAxis yAxis, VirtualButton attackButton, VirtualButton specialButton);
+        private SpriteRenderer _sprite;
+        protected abstract SpriteRenderer loadSprite();
+
+        public FighterState()
+        {
+            _sprite = loadSprite();
+        }
+
+        public override void OnAddedToEntity()
+        {
+            this.AddComponent(_sprite);
+        }
+
+        public override void OnRemovedFromEntity()
+        {
+            this.RemoveComponent(_sprite);
+        }
+    }
+}
