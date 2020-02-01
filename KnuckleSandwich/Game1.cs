9c1eb01bdcbae058d3551b6755dd1f7210263c98
Bret Gourdie <BretGourdie@gmail.com>
2020-01-31 21:06:50 -0500

    Trying to create a scene, null GraphicsDevice

--- KnuckleSandwich/Game1.cs
+++ KnuckleSandwich/Game1.cs
@@ -1,14 +1,27 @@
-﻿namespace KnuckleSandwich
+﻿using KnuckleSandwich.Gameplay;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Nez;
+using Nez.Sprites;
+using Nez.Textures;
+
+namespace KnuckleSandwich
 {
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
     public class Game1 : Nez.Core
     {
+        public Game1() : base(
+            width: 800,
+            height: 600,
+            windowTitle: "Knuckle Sandwich")
+        { }
+
         protected override void Initialize()
         {
-            base.Initialize();
-            // Scene = new StartScene();
+            var gameplayScene = new GameplayScene();
+            Scene = gameplayScene;
         }
     }
 }
---
+++ KnuckleSandwich/Gameplay/GameplayScene.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Nez;
+using Nez.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnuckleSandwich.Gameplay
+{
+    public class GameplayScene : Scene
+    {
+        public override void Initialize()
+        {
+        }
+
+        public override void OnStart()
+        {
+            var bread = new Entity("bread");
+            var texture = Content.Load<Texture2D>(@"Sprites\WhiteBread");
+            var sprite = new SpriteRenderer(texture);
+            bread.AddComponent(sprite);
+
+            AddEntity(bread);
+        }
+    }
+}
